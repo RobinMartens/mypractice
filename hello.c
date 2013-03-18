dd5dc2ff40049b18524df9a26665da740e908ada
Robin Martens <robin_martens@brown.edu>
2013-03-18 15:55:54 -0400

    Needed to change 'hello.c'

--- hello.c
+++ hello.c
@@ -5,6 +5,6 @@ int main(int argc, char **argv){
 	// I forgot to include this comment
 	// Oh, and this one!
 	printf("Hello, world!\n");
-	printf("I am a C program.\n");
+	printf("I am an executible.\n");
 
 }
